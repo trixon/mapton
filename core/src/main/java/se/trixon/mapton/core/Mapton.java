3509ff4d41669ca548a498272f8f3b8dc5beb9a3
Patrik Karlström <patrik@trixon.se>
2018-04-26 20:46:28 +0200

    Add NbLog call

---
+++ core/src/main/java/se/trixon/mapton/core/Mapton.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.mapton.core;
+
+import java.awt.Frame;
+import java.text.DateFormat;
+import org.openide.awt.StatusDisplayer;
+import org.openide.windows.WindowManager;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+public class Mapton {
+
+    public static final String LOG_TAG = "Mapton";
+
+    public static void clearStatusText() {
+        setStatusText("");
+    }
+
+    public static DateFormat getDefaultDateFormat() {
+        return DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM);
+    }
+
+    public static Frame getFrame() {
+        return WindowManager.getDefault().getMainWindow();
+    }
+
+    public static void setStatusText(String text, int importance) {
+        StatusDisplayer.getDefault().setStatusText(text, importance);
+    }
+
+    public static void setStatusText(String text) {
+        setStatusText(text, StatusDisplayer.IMPORTANCE_ANNOTATION);
+    }
+}
