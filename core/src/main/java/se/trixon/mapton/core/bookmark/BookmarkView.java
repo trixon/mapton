17abf53d3fbaac24ab235f06347721dc0d7d11bc
Patrik Karlström <patrik@trixon.se>
2018-04-29 22:34:25 +0200

    Confirm bookmark removal

--- core/src/main/java/se/trixon/mapton/core/bookmark/BookmarkView.java
+++ core/src/main/java/se/trixon/mapton/core/bookmark/BookmarkView.java
@@ -17,6 +17,7 @@ package se.trixon.mapton.core.bookmark;
 
 import java.util.Arrays;
 import java.util.Collection;
+import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ContextMenu;
@@ -30,11 +31,14 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javax.swing.SwingUtilities;
 import org.controlsfx.control.action.Action;
 import org.controlsfx.control.action.ActionUtils;
 import org.controlsfx.glyphfont.FontAwesome;
 import org.controlsfx.glyphfont.GlyphFont;
 import org.controlsfx.glyphfont.GlyphFontRegistry;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
 import se.trixon.almond.util.Dict;
 import se.trixon.almond.util.SystemHelper;
 
@@ -67,34 +71,63 @@ public class BookmarkView extends BorderPane {
         //TODO Add bookmark search
     }
 
-    private void bookmarkEdit() {
-        Bookmark bookmark = getSelectedBookmark();
-        mManager.editBookmark(bookmark);
-    }
-
     private void bookmarkCopy() {
         Bookmark bookmark = getSelectedBookmark();
         SystemHelper.copyToClipboard(String.format("geo:%f,%f", bookmark.getLatitude(), bookmark.getLongitude()));
     }
 
-    private void bookmarkRemove() {
+    private void bookmarkEdit() {
         Bookmark bookmark = getSelectedBookmark();
-        //TODO Add confirm dialog
-        mListView.getItems().remove(bookmark);
-    }
-
-    private void bookmarkRemoveAll() {
-        //TODO Add confirm dialog
-        mListView.getItems().clear();
-    }
-
-    private Bookmark getSelectedBookmark() {
-        return mListView.getSelectionModel().getSelectedItem();
+        mManager.editBookmark(bookmark);
     }
 
     private void bookmarkGoTo() {
         //TODO Pan map
         System.out.println("GO TO BOOKMARK " + System.currentTimeMillis());
+    }
+
+    private void bookmarkRemove() {
+        final Bookmark bookmark = getSelectedBookmark();
+
+        SwingUtilities.invokeLater(() -> {
+            String[] buttons = new String[]{Dict.CANCEL.toString(), Dict.REMOVE.toString()};
+            NotifyDescriptor d = new NotifyDescriptor(
+                    String.format(Dict.Dialog.MESSAGE_PROFILE_REMOVE.toString(), bookmark.getName()),
+                    Dict.Dialog.TITLE_BOOKMARK_REMOVE.toString() + "?",
+                    NotifyDescriptor.OK_CANCEL_OPTION,
+                    NotifyDescriptor.WARNING_MESSAGE,
+                    buttons,
+                    Dict.REMOVE.toString());
+
+            if (Dict.REMOVE.toString() == DialogDisplayer.getDefault().notify(d)) {
+                Platform.runLater(() -> {
+                    mListView.getItems().remove(bookmark);
+                });
+            }
+        });
+    }
+
+    private void bookmarkRemoveAll() {
+        SwingUtilities.invokeLater(() -> {
+            String[] buttons = new String[]{Dict.CANCEL.toString(), Dict.REMOVE_ALL.toString()};
+            NotifyDescriptor d = new NotifyDescriptor(
+                    Dict.Dialog.MESSAGE_BOOKMARK_REMOVE_ALL.toString(),
+                    Dict.Dialog.TITLE_BOOKMARK_REMOVE_ALL.toString() + "?",
+                    NotifyDescriptor.OK_CANCEL_OPTION,
+                    NotifyDescriptor.WARNING_MESSAGE,
+                    buttons,
+                    Dict.REMOVE_ALL.toString());
+
+            if (Dict.REMOVE_ALL.toString() == DialogDisplayer.getDefault().notify(d)) {
+                Platform.runLater(() -> {
+                    mListView.getItems().clear();
+                });
+            }
+        });
+    }
+
+    private Bookmark getSelectedBookmark() {
+        return mListView.getSelectionModel().getSelectedItem();
     }
 
     class BookmarkListCell extends ListCell<Bookmark> {
