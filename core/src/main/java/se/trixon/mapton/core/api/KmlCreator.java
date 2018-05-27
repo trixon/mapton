a23f4c9ecf5cf16c4c8531d3056aee0b7407c56f
Patrik Karlström <patrik@trixon.se>
2018-05-27 06:55:40 +0200

    Create ExportBookmarksToKmlAction

--- core/src/main/java/se/trixon/mapton/core/api/KmlCreator.java
+++ core/src/main/java/se/trixon/mapton/core/api/KmlCreator.java
@@ -23,9 +23,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.logging.Logger;
-import javafx.geometry.Point2D;
 import org.apache.commons.io.FileUtils;
-import org.openide.util.Exceptions;
 
 /**
  *
@@ -36,27 +34,20 @@ public abstract class KmlCreator {
     protected static final Logger LOGGER = Logger.getLogger(KmlCreator.class.getName());
     protected final Document mDocument;
     protected final Kml mKml = new Kml();
-    protected final Folder mRootFolder;
+    protected Folder mRootFolder;
     protected final SimpleDateFormat mTimeStampDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
 
-    public KmlCreator(String name) {
+    public KmlCreator() {
         mDocument = mKml.createAndSetDocument().withOpen(true);
-        mRootFolder = mDocument.createAndAddFolder().withName(name).withOpen(true);
     }
 
-    public String save(File f) {
+    public String save(File f) throws IOException {
         StringWriter stringWriter = new StringWriter();
         mKml.marshal(stringWriter);
         String kmlString = stringWriter.toString();
 
-        try {
-            FileUtils.writeStringToFile(f, kmlString, "utf-8");
-        } catch (IOException ex) {
-            Exceptions.printStackTrace(ex);
-        }
+        FileUtils.writeStringToFile(f, kmlString, "utf-8");
 
         return kmlString;
     }
-
-    public abstract Point2D toWgs84(double latitude, double longitude);
 }
