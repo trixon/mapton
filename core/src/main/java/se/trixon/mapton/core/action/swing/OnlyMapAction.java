3fba72cd67fab6a4d842daa0f440edbf9ee56e23
Patrik Karlström <patrik@trixon.se>
2018-08-26 07:19:27 +0200

    Update license header

--- core/src/main/java/se/trixon/mapton/core/action/swing/OnlyMapAction.java
+++ core/src/main/java/se/trixon/mapton/core/action/swing/OnlyMapAction.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2018 pata.
+/* 
+ * Copyright 2018 Patrik Karlström.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
