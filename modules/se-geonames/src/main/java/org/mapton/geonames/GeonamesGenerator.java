bd42b90c03572c5368b2d9a838ab10cbfca4f53a
Patrik Karlström <patrik@trixon.se>
2018-11-03 12:28:28 +0100

    Add Geonames off line search engine

---
+++ modules/se-geonames/src/main/java/org/mapton/geonames/GeonamesGenerator.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2018 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.geonames;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.io.FileUtils;
+import se.trixon.almond.util.MathHelper;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+public class GeonamesGenerator {
+
+    private final File mBaseDir;
+    private final ArrayList<Geoname> mGeonames = new ArrayList<>();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        new GeonamesGenerator();
+    }
+
+    public GeonamesGenerator() throws IOException {
+        mBaseDir = FileUtils.getUserDirectory();
+        populateCities();
+
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+
+        FileUtils.write(new File(mBaseDir, "geonames.json"), gson.toJson(mGeonames), "utf-8");
+    }
+
+    private void populateCities() throws IOException {
+        //http://download.geonames.org/export/dump/cities1000.zip
+        try (CSVParser records = CSVParser.parse(
+                new File(mBaseDir, "cities1000.txt"),
+                Charset.forName("utf-8"),
+                CSVFormat.DEFAULT.withDelimiter('\t')
+        )) {
+            for (CSVRecord record : records) {
+                String name = record.get(1);
+                String asciiname = record.get(2);
+                String alternateames = record.get(3);
+                Double lat = MathHelper.convertStringToDouble(record.get(4));
+                Double lon = MathHelper.convertStringToDouble(record.get(5));
+                String countryCode = record.get(8);
+
+                Geoname geoname = new Geoname();
+
+                geoname.setName(name);
+                geoname.setAsciiName(asciiname);
+                geoname.setAlternateNames(alternateames);
+                geoname.setCountryCode(countryCode);
+                geoname.setLatitude(lat);
+                geoname.setLongitude(lon);
+
+                mGeonames.add(geoname);
+            }
+        }
+    }
+}
---
+++ modules/se-geonames/src/main/java/org/mapton/geonames/GeonamesSearchEngine.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2018 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.geonames;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
+import java.util.ArrayList;
+import java.util.TreeMap;
+import org.apache.commons.lang.StringUtils;
+import org.mapton.api.MBookmark;
+import org.mapton.api.MSearchEngine;
+import org.openide.util.lookup.ServiceProvider;
+import se.trixon.almond.util.SystemHelper;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+@ServiceProvider(service = MSearchEngine.class)
+public class GeonamesSearchEngine implements MSearchEngine {
+
+    private final TreeMap<String, String> mCountries = new TreeMap<>();
+
+    private ArrayList<Geoname> mGeonames;
+    private final Gson mGson = new GsonBuilder().create();
+
+    public GeonamesSearchEngine() {
+        init();
+    }
+
+    @Override
+    public String getName() {
+        return "GeoNames";
+    }
+
+    @Override
+    public ArrayList<MBookmark> getResults(String searchString) {
+        ArrayList<MBookmark> bookmarks = new ArrayList<>();
+
+        mGeonames.stream()
+                .filter((g) -> (StringUtils.containsIgnoreCase(String.join("/", g.getAsciiName(), g.getName(), g.getAlternateNames(), mCountries.getOrDefault(g.getCountryCode(), "")), searchString)))
+                .forEachOrdered((g) -> {
+                    MBookmark b = new MBookmark();
+                    b.setName(g.getName());
+                    b.setCategory(g.getCountryCode());
+                    b.setLatitude(g.getLatitude());
+                    b.setLongitude(g.getLongitude());
+                    bookmarks.add(b);
+                });
+
+        return bookmarks;
+    }
+
+    private void init() {
+        String json;
+
+        json = SystemHelper.getResourceAsString(GeonamesSearchEngine.class, "geonames.json");
+        mGeonames = mGson.fromJson(json, new TypeToken<ArrayList<Geoname>>() {
+        }.getType());
+
+        mGeonames.sort((Geoname o1, Geoname o2) -> o1.getName().compareTo(o2.getName()));
+        json = SystemHelper.getResourceAsString(GeonamesSearchEngine.class, "country_codes.json");
+        ArrayList<Country> countries = mGson.fromJson(json, new TypeToken<ArrayList<Country>>() {
+        }.getType());
+
+        countries.forEach((country) -> {
+            mCountries.put(country.mCode, country.mName);
+        });
+    }
+
+    private class Country {
+
+        @SerializedName("Code")
+        private String mCode;
+        @SerializedName("Name")
+        private String mName;
+    }
+}
