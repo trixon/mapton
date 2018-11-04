8f2fb5bbb2cdaa17cdbe50258260629b5853bc79
Patrik Karlström <patrik@trixon.se>
2018-11-04 15:39:50 +0100

    Rename module

---
+++ modules/te-swetrans/src/main/java/org/mapton/swetrans/SW991200.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.swetrans;
+
+import com.github.goober.coordinatetransformation.positions.SWEREF99Position.SWEREFProjection;
+import org.openide.util.lookup.ServiceProvider;
+import org.mapton.api.MBounds;
+import org.mapton.api.MCooTrans;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+@ServiceProvider(service = MCooTrans.class)
+public class SW991200 extends BaseSR {
+
+    public SW991200() {
+        mName = "SWEREF 99 12 00";
+        mProjection = SWEREFProjection.sweref_99_12_00;
+        mBoundsWgs84 = new MBounds(10.5700, 55.8500, 13.0800, 60.2500);
+        mBoundsProjected = new MBounds(60436.5084, 6192389.5650, 217643.4713, 6682784.4276);
+    }
+}
---
+++ modules/te-swetrans/src/main/java/org/mapton/swetrans/SW992015.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.swetrans;
+
+import com.github.goober.coordinatetransformation.positions.SWEREF99Position.SWEREFProjection;
+import org.openide.util.lookup.ServiceProvider;
+import org.mapton.api.MBounds;
+import org.mapton.api.MCooTrans;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+@ServiceProvider(service = MCooTrans.class)
+public class SW992015 extends BaseSR {
+
+    public SW992015() {
+        mName = "SWEREF 99 20 15";
+        mProjection = SWEREFProjection.sweref_99_20_15;
+        mBoundsWgs84 = new MBounds(15.3800, 63.4000, 22.4800, 69.1000);
+        mBoundsProjected = new MBounds(-93218.3385, 7034909.8738, 261434.6246, 7676279.8691);
+    }
+}
