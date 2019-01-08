da0c4f7f7d8903999f43cdedd6b8664f1b1901e0
Patrik Karlström <patrik@trixon.se>
2019-01-08 15:48:56 +0100

    Update copyright year

--- modules/wikipedia/src/main/java/org/mapton/wikipedia/result/Continue.java
+++ modules/wikipedia/src/main/java/org/mapton/wikipedia/result/Continue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Patrik Karlström.
+ * Copyright 2019 Patrik Karlström.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
