ebeffe3ff37e9bea5ecd0f5eb28460e743670671
Patrik Karlström <patrik@trixon.se>
2019-10-19 09:00:35 +0200

    Move finalizer from api to base

--- base/src/main/java/org/mapton/base/Finalizer.java
+++ base/src/main/java/org/mapton/base/Finalizer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.mapton.impl;
+package org.mapton.base;
 
 import org.mapton.api.MEngine;
 import org.mapton.api.MOptions;
