0599562da717f052d75a19223c4ad228b2a59ad0
Patrik Karlström <patrik@trixon.se>
2019-12-30 19:51:13 +0100

    xkcd: Add xkcd reader

---
+++ app-nb/modules-add-ons/xkcd-nb/src/main/java/org/mapton/addon/xkcd_nb/XkcdManager.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2019 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.addon.xkcd_nb;
+
+import java.io.IOException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.mapton.api.MBackgroundImage;
+import org.mapton.api.MKey;
+import org.mapton.api.Mapton;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+public class XkcdManager {
+
+    private static final String BASE_URL = "https://xkcd.com/";
+    private Notifier mDisplayer;
+    private int mIndex;
+    private String mUrl;
+
+    public static void main(String[] args) throws IOException {
+        Xkcd x = new XkcdManager().parse();
+        System.out.println(ToStringBuilder.reflectionToString(x, ToStringStyle.MULTI_LINE_STYLE));
+    }
+
+    public void setDisplayer(Notifier displayer) {
+        mDisplayer = displayer;
+    }
+
+    void go(Xkcd xkcd) {
+        Mapton.getGlobalState().put(MKey.BACKGROUND_IMAGE, new MBackgroundImage(xkcd.getSrc(), .85));
+        mDisplayer.onShow(xkcd);
+    }
+
+    void goFirst() {
+        new Thread(() -> {
+            try {
+                go(parse(mIndex = 1));
+            } catch (IOException ex) {
+                //Exceptions.printStackTrace(ex);
+            }
+        }).start();
+    }
+
+    void goLast() {
+        new Thread(() -> {
+            try {
+                go(parse());
+            } catch (IOException ex) {
+                //Exceptions.printStackTrace(ex);
+            }
+        }).start();
+    }
+
+    void goNext() {
+        new Thread(() -> {
+            try {
+                go(parse(++mIndex));
+            } catch (IOException ex) {
+                //Exceptions.printStackTrace(ex);
+            }
+        }).start();
+    }
+
+    void goPrevious() {
+        new Thread(() -> {
+            try {
+                go(parse(--mIndex));
+            } catch (IOException ex) {
+                //Exceptions.printStackTrace(ex);
+            }
+        }).start();
+    }
+
+    void goRandom() {
+        new Thread(() -> {
+            try {
+                go(parse("https://c.xkcd.com/random/comic/"));
+            } catch (IOException ex) {
+                //Exceptions.printStackTrace(ex);
+            }
+        }).start();
+    }
+
+    private Xkcd parse(int number) throws IOException {
+        return parse(String.format("%s%d", BASE_URL, number));
+    }
+
+    private Xkcd parse(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        //System.out.println(doc.toString());
+
+        Elements metaTags = doc.getElementsByTag("meta");
+        for (Element metaTag : metaTags) {
+            if (StringUtils.equalsIgnoreCase(metaTag.attr("property"), "og:url")) {
+                mUrl = metaTag.attr("content");
+                mIndex = Integer.parseInt(mUrl.replaceAll("\\D+", ""));
+            }
+        }
+
+        Element comic = doc.getElementById("comic");
+        Element imageElement = comic.select("img").first();
+
+        Xkcd xkcd = new Xkcd();
+        xkcd.setAlt(imageElement.attr("alt"));
+        xkcd.setFootnote(doc.getElementById("footnote").text());
+        xkcd.setLicense(doc.getElementById("licenseText").text());
+        xkcd.setSrc(imageElement.absUrl("src"));
+        xkcd.setTitle(imageElement.attr("title"));
+        xkcd.setUrl(mUrl);
+
+        return xkcd;
+    }
+
+    private Xkcd parse() throws IOException {
+        return parse(BASE_URL);
+    }
+
+    public interface Notifier {
+
+        void onShow(Xkcd xkcd);
+    }
+}
---
+++ app-nb/modules-add-ons/xkcd-nb/src/main/java/org/mapton/addon/xkcd_nb/XkcdTool.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.addon.xkcd_nb;
+
+import org.controlsfx.control.action.Action;
+import org.mapton.api.MToolMap;
+import org.openide.util.lookup.ServiceProvider;
+import se.trixon.almond.nbp.Almond;
+import se.trixon.almond.util.Dict;
+import se.trixon.almond.util.fx.FxActionSwing;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+@ServiceProvider(service = MToolMap.class)
+public class XkcdTool implements MToolMap {
+
+    public static final String NAME = "XKCD";
+
+    @Override
+    public Action getAction() {
+        FxActionSwing action = new FxActionSwing(NAME, () -> {
+            Almond.openAndActivateTopComponent("XkcdTopComponent");
+        });
+
+        return action;
+    }
+
+    @Override
+    public String getParent() {
+        return Dict.ADD_ONS.toString();
+    }
+}
