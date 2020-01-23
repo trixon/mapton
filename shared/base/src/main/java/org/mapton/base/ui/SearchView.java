841e04aa5eef69034a1f28fb1eef4bbd2773ed80
Patrik Karlström <patrik@trixon.se>
2020-01-23 15:37:24 +0100

    base: Fix search view icon color

--- shared/base/src/main/java/org/mapton/base/ui/SearchView.java
+++ shared/base/src/main/java/org/mapton/base/ui/SearchView.java
@@ -100,7 +100,7 @@ public class SearchView {
 
     private void createUI() {
         mSearchTextField = (CustomTextField) TextFields.createClearableTextField();
-        mSearchTextField.setLeft(MaterialIcon._Action.SEARCH.getImageView(getIconSizeToolBarInt() - FxHelper.getUIScaled(4)));
+        mSearchTextField.setLeft(MaterialIcon._Action.SEARCH.getImageView(getIconSizeToolBarInt() - FxHelper.getUIScaled(4), mOptions.getIconColorBright()));
         mSearchTextField.setPromptText(MDict.SEARCH_PROMPT.toString());
         mSearchTextField.setPrefColumnCount(25);
         mSearchTextField.setText("");
