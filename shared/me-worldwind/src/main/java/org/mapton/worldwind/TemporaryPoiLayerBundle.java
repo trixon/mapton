1caf24cbef1db1bedd38ba647c0473aeb231d2c2
Patrik Karlström <patrik@trixon.se>
2020-02-28 17:09:33 +0100

    api: Add TemporaryPoiManager and ww layer bundle

---
+++ shared/me-worldwind/src/main/java/org/mapton/worldwind/TemporaryPoiLayerBundle.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2019 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.worldwind;
+
+import gov.nasa.worldwind.WorldWind;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.layers.RenderableLayer;
+import gov.nasa.worldwind.render.PointPlacemark;
+import gov.nasa.worldwind.render.PointPlacemarkAttributes;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javafx.collections.ListChangeListener;
+import org.mapton.api.MBookmark;
+import org.mapton.api.MKey;
+import org.mapton.api.MPoi;
+import org.mapton.api.MTemporaryPoiManager;
+import org.mapton.api.Mapton;
+import org.mapton.worldwind.api.LayerBundle;
+import org.mapton.worldwind.api.LayerBundleManager;
+import org.mapton.worldwind.api.WWHelper;
+import org.openide.util.lookup.ServiceProvider;
+import se.trixon.almond.util.Dict;
+import se.trixon.almond.util.fx.FxHelper;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+@ServiceProvider(service = LayerBundle.class)
+public class TemporaryPoiLayerBundle extends LayerBundle {
+
+    private final MTemporaryPoiManager mManager = MTemporaryPoiManager.getInstance();
+    private final RenderableLayer mLayer = new RenderableLayer();
+
+    public TemporaryPoiLayerBundle() {
+        init();
+        initListeners();
+    }
+
+    @Override
+    public void populate() throws Exception {
+        getLayers().add(mLayer);
+        setPopulated(true);
+    }
+
+    private void init() {
+        mLayer.setName(Dict.TEMPORARY_CONTENT.toString());
+        setCategorySystem(mLayer);
+        setName(Dict.TEMPORARY_CONTENT.toString());
+        mLayer.setEnabled(true);
+        mLayer.setPickEnabled(false);
+    }
+
+    private void initListeners() {
+        mManager.getItems().addListener((ListChangeListener.Change<? extends MBookmark> c) -> {
+            updatePlacemarks();
+        });
+    }
+
+    private void updatePlacemarks() {
+        mLayer.removeAllRenderables();
+
+        for (MPoi poi : mManager.getItems()) {
+            if (poi.isDisplayMarker()) {
+                PointPlacemark placemark = new PointPlacemark(Position.fromDegrees(poi.getLatitude(), poi.getLongitude()));
+                placemark.setLabelText(poi.getName());
+                placemark.setAltitudeMode(WorldWind.CLAMP_TO_GROUND);
+                placemark.setEnableLabelPicking(true);
+
+                PointPlacemarkAttributes attrs = new PointPlacemarkAttributes(placemark.getDefaultAttributes());
+                attrs.setImageAddress("images/pushpins/plain-white.png");
+                attrs.setImageColor(FxHelper.colorToColor(FxHelper.colorFromHexRGBA(poi.getColor())));
+                placemark.setAttributes(attrs);
+                placemark.setHighlightAttributes(WWHelper.createHighlightAttributes(attrs, 1.5));
+
+                placemark.setValue(WWHelper.KEY_RUNNABLE_LEFT_CLICK, (Runnable) () -> {
+                    Map<String, Object> propertyMap = new LinkedHashMap<>();
+                    propertyMap.put(Dict.NAME.toString(), poi.getName());
+                    propertyMap.put(Dict.DESCRIPTION.toString(), poi.getDescription());
+                    propertyMap.put(Dict.CATEGORY.toString(), poi.getCategory());
+                    propertyMap.put(Dict.COLOR.toString(), javafx.scene.paint.Color.web(poi.getColor()));
+
+                    Mapton.getGlobalState().put(MKey.OBJECT_PROPERTIES, propertyMap);
+                });
+
+                mLayer.addRenderable(placemark);
+            }
+        }
+
+        LayerBundleManager.getInstance().redraw();
+    }
+}
