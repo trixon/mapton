29e0577376fb714b183a09f83b83fc8ad005cd16
Patrik Karlström <patrik@trixon.se>
2020-08-03 20:06:16 +0200

    trv-ti: Use MUpdater

---
+++ add-ons/poi/trv-traffic-information/src/main/java/org/mapton/poi_trv_ti/updater/BaseUpdater.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.poi_trv_ti.updater;
+
+import org.mapton.api.MPoiManager;
+import org.mapton.api.MUpdater;
+import org.mapton.poi_trv_ti.TrafficInformationManager;
+import se.trixon.trv_traffic_information.TrafficInformation;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+public abstract class BaseUpdater extends MUpdater.ByFile {
+
+    protected final TrafficInformationManager mManager = TrafficInformationManager.getInstance();
+    protected final TrafficInformation mTrafficInformation = mManager.getTrafficInformation();
+
+    public BaseUpdater() {
+        setAutoUpdate(true);
+        setCategory("Trv Traffic Information");
+    }
+
+    public void refreshPoiManager() {
+        MPoiManager.getInstance().refresh();
+    }
+
+}
---
+++ add-ons/poi/trv-traffic-information/src/main/java/org/mapton/poi_trv_ti/updater/TrafficSafetyCameraUpdater.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.poi_trv_ti.updater;
+
+import java.io.IOException;
+import javax.xml.bind.JAXBException;
+import org.mapton.api.MUpdater;
+import org.mapton.poi_trv_ti.TrafficInformationManager.Service;
+import org.openide.util.Exceptions;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+@ServiceProvider(service = MUpdater.class)
+public class TrafficSafetyCameraUpdater extends BaseUpdater {
+
+    public TrafficSafetyCameraUpdater() {
+        setFile(mManager.getFile(Service.TRAFFIC_SAFETY_CAMERA));
+        setComment("Speed cams");
+
+        setRunnable(() -> {
+            try {
+                mTrafficInformation.road().getTrafficSafetyCameraResults(null, null, mManager.getFile(Service.TRAFFIC_SAFETY_CAMERA));
+                refreshPoiManager();
+            } catch (IOException | InterruptedException | JAXBException ex) {
+                mPrint.err(ex.getMessage());
+                Exceptions.printStackTrace(ex);
+            }
+        });
+
+        setAutoUpdateInterval(FREQ_1_WEEK);
+        initAutoUpdater();
+    }
+
+    @Override
+    public String getName() {
+        return "Traffic Safety Camera";
+    }
+}
