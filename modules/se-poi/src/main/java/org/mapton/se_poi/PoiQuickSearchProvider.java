7376f7f50889d6eceb64f622937ae08c7a1d4164
Patrik Karlström <patrik@trixon.se>
2020-09-04 08:59:34 +0200

    se-poi: Tweak QuickSearchProvider

--- modules/se-poi/src/main/java/org/mapton/se_poi/PoiQuickSearchProvider.java
+++ modules/se-poi/src/main/java/org/mapton/se_poi/PoiQuickSearchProvider.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2020 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.mapton.se_poi;
 
@@ -14,6 +24,10 @@ import org.netbeans.spi.quicksearch.SearchProvider;
 import org.netbeans.spi.quicksearch.SearchRequest;
 import org.netbeans.spi.quicksearch.SearchResponse;
 
+/**
+ *
+ * @author Patrik Karlström
+ */
 public class PoiQuickSearchProvider implements SearchProvider {
 
     @Override
@@ -26,7 +40,6 @@ public class PoiQuickSearchProvider implements SearchProvider {
                     break;
                 }
             }
-
         }
     }
 }
