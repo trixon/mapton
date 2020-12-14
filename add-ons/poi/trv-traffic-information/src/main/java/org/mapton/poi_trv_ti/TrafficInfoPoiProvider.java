19ef86c705e38354901d6ad0f0924b241980d8a9
Patrik Karlström <patrik@trixon.se>
2020-12-14 19:16:15 +0100

    trv-ti: Improve poi loading

--- add-ons/poi/trv-traffic-information/src/main/java/org/mapton/poi_trv_ti/TrafficInfoPoiProvider.java
+++ add-ons/poi/trv-traffic-information/src/main/java/org/mapton/poi_trv_ti/TrafficInfoPoiProvider.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import org.apache.commons.lang3.StringUtils;
-import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.WKTReader;
 import org.mapton.api.MPoi;
@@ -30,11 +29,6 @@ import org.openide.util.NbBundle;
 import org.openide.util.lookup.ServiceProvider;
 import se.trixon.almond.util.SystemHelper;
 import se.trixon.almond.util.fx.FxHelper;
-import se.trixon.trv_traffic_information.road.camera.v1.Camera;
-import se.trixon.trv_traffic_information.road.parking.v1_4.Parking;
-import se.trixon.trv_traffic_information.road.trafficsafetycamera.v1.TrafficSafetyCamera;
-import se.trixon.trv_traffic_information.road.weatherstation.v1.Measurement;
-import se.trixon.trv_traffic_information.road.weatherstation.v1.WeatherStation;
 
 /**
  *
@@ -61,7 +55,7 @@ public class TrafficInfoPoiProvider implements MPoiProvider {
 
     @Override
     public ArrayList<MPoi> getPois() {
-        ArrayList<MPoi> pois = new ArrayList<>();
+        var pois = new ArrayList<MPoi>();
         addCameras(pois);
         addParking(pois);
         addTrafficSafetyCameras(pois);
@@ -73,120 +67,139 @@ public class TrafficInfoPoiProvider implements MPoiProvider {
     private void addCameras(ArrayList<MPoi> pois) {
         mManager.getCameraGroupToPhotoUrl().clear();
         mManager.getResultsCamera().forEach(result -> {
-            for (Camera camera : result.getCamera()) {
-                if (!camera.isActive()) {
-                    continue;
-                }
+            for (var camera : result.getCamera()) {
                 try {
-                    mManager.getCameraGroupToPhotoUrl().put(camera.getCameraGroup(), camera.getPhotoUrl());
-                } catch (NullPointerException e) {
-                    //System.out.println(ToStringBuilder.reflectionToString(camera, ToStringStyle.MULTI_LINE_STYLE));
-                }
-                MPoi poi = new MPoi();
-                poi.setDescription(camera.getDescription());
-                poi.setCategory(String.format("%s", "Kameror"));
-                poi.setCategory(camera.getType());
-                poi.setColor("ff0000");
-                poi.setDisplayMarker(true);
-                poi.setName(camera.getName());
-                poi.setZoom(0.9);
-                poi.setExternalImageUrl(camera.getPhotoUrl() + "?type=fullsize");
-                setLatLonFromGeometry(poi, camera.getGeometry().getWGS84());
-                final MPoiStyle style = new MPoiStyle();
-                poi.setStyle(style);
-                style.setImageUrl(getPlacemarkUrl(camera.getIconId()));
-                style.setLabelVisible(false);
+                    if (!camera.isActive()) {
+                        continue;
+                    }
+                    try {
+                        mManager.getCameraGroupToPhotoUrl().put(camera.getCameraGroup(), camera.getPhotoUrl());
+                    } catch (NullPointerException e) {
+                        //System.out.println(ToStringBuilder.reflectionToString(camera, ToStringStyle.MULTI_LINE_STYLE));
+                    }
+                    var poi = new MPoi();
+                    poi.setDescription(camera.getDescription());
+                    poi.setCategory(String.format("%s", "Kameror"));
+                    poi.setCategory(camera.getType());
+                    poi.setColor("ff0000");
+                    poi.setDisplayMarker(true);
+                    poi.setName(camera.getName());
+                    poi.setZoom(0.9);
+                    poi.setExternalImageUrl(camera.getPhotoUrl() + "?type=fullsize");
+                    setLatLonFromGeometry(poi, camera.getGeometry().getWGS84());
 
-                pois.add(poi);
+                    var style = new MPoiStyle();
+                    poi.setStyle(style);
+                    style.setImageUrl(getPlacemarkUrl(camera.getIconId()));
+                    style.setLabelVisible(false);
+
+                    pois.add(poi);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
             }
         });
     }
 
     private void addParking(ArrayList<MPoi> pois) {
         mManager.getResultsParking().forEach(result -> {
-            for (Parking parking : result.getParking()) {
-                MPoi poi = new MPoi();
-                poi.setDescription(parking.getDescription());
-                poi.setCategory(String.format("%s", "Parkering"));
-                poi.setColor("00ff00");
-                poi.setDisplayMarker(true);
-                poi.setName(parking.getName());
-                poi.setZoom(0.9);
-                setLatLonFromGeometry(poi, parking.getGeometry().getWGS84());
-                final MPoiStyle style = new MPoiStyle();
-                poi.setStyle(style);
-                style.setImageUrl(getPlacemarkUrl(parking.getIconId()));
-                style.setLabelVisible(false);
+            for (var parking : result.getParking()) {
+                try {
+                    var poi = new MPoi();
+                    poi.setDescription(parking.getDescription());
+                    poi.setCategory(String.format("%s", "Parkering"));
+                    poi.setColor("00ff00");
+                    poi.setDisplayMarker(true);
+                    poi.setName(parking.getName());
+                    poi.setZoom(0.9);
+                    setLatLonFromGeometry(poi, parking.getGeometry().getWGS84());
 
-                pois.add(poi);
+                    var style = new MPoiStyle();
+                    poi.setStyle(style);
+                    style.setImageUrl(getPlacemarkUrl(parking.getIconId()));
+                    style.setLabelVisible(false);
+
+                    pois.add(poi);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
             }
         });
     }
 
     private void addTrafficSafetyCameras(ArrayList<MPoi> pois) {
         mManager.getResultsTrafficSafetyCamera().forEach(result -> {
-            for (TrafficSafetyCamera camera : result.getTrafficSafetyCamera()) {
-                MPoi poi = new MPoi();
-                poi.setDescription(camera.getBearing().toString());
-                poi.setCategory(String.format("%s", "Trafiksäkerhetskameror"));
-                poi.setColor("00ff00");
-                poi.setDisplayMarker(true);
-                poi.setName(camera.getName());
-                poi.setZoom(0.9);
-                setLatLonFromGeometry(poi, camera.getGeometry().getWGS84());
-                final MPoiStyle style = new MPoiStyle();
-                poi.setStyle(style);
-                style.setImageUrl(getPlacemarkUrl(camera.getIconId()));
-                style.setLabelVisible(false);
+            for (var camera : result.getTrafficSafetyCamera()) {
+                try {
+                    var poi = new MPoi();
+                    poi.setDescription(camera.getBearing().toString());
+                    poi.setCategory(String.format("%s", "Trafiksäkerhetskameror"));
+                    poi.setColor("00ff00");
+                    poi.setDisplayMarker(true);
+                    poi.setName(camera.getName());
+                    poi.setZoom(0.9);
+                    setLatLonFromGeometry(poi, camera.getGeometry().getWGS84());
 
-                pois.add(poi);
+                    var style = new MPoiStyle();
+                    poi.setStyle(style);
+                    style.setImageUrl(getPlacemarkUrl(camera.getIconId()));
+                    style.setLabelVisible(false);
+
+                    pois.add(poi);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
             }
         });
     }
 
     private void addWeatherStations(ArrayList<MPoi> pois) {
         mManager.getResultsWeatherStation().forEach(result -> {
-            for (WeatherStation weatherStation : result.getWeatherStation()) {
-                if ( //                        (weatherStation.getRoadNumberNumeric() != null && weatherStation.getRoadNumberNumeric() > 0)
-                        //                        (weatherStation.isActive() != null && !weatherStation.isActive())
-                        (weatherStation.isDeleted() != null && weatherStation.isDeleted())
-                        || StringUtils.endsWith(weatherStation.getName(), " Fjärryta")) {
-                    continue;
-                }
-
-                MPoi poi = new MPoi();
-                poi.setCategory(String.format("%s", "Väderstation"));
-                poi.setDisplayMarker(true);
-                poi.setName(weatherStation.getName());
-                poi.setZoom(0.9);
-                poi.setPropertyNode(mWeatherView);
-                poi.setPropertySource(weatherStation);
-                setLatLonFromGeometry(poi, weatherStation.getGeometry().getWGS84());
-
-                final MPoiStyle style = new MPoiStyle();
-                poi.setStyle(style);
-                final Measurement measurement = weatherStation.getMeasurement();
-                if (weatherStation.isActive()) {
-                    style.setLabelText(String.format("%.0f°", measurement.getAir().getTemp()));
-                    style.setImageUrl(mManager.getIconUrl(measurement.getPrecipitation()));
-                    try {
-                        poi.setDescription(String.format("%s %s",
-                                measurement.getPrecipitation().getAmount(),
-                                measurement.getPrecipitation().getAmountName()
-                        ));
-                    } catch (Exception e) {
+            for (var weatherStation : result.getWeatherStation()) {
+                try {
+                    if ( //                        (weatherStation.getRoadNumberNumeric() != null && weatherStation.getRoadNumberNumeric() > 0)
+                            //                        (weatherStation.isActive() != null && !weatherStation.isActive())
+                            (weatherStation.isDeleted() != null && weatherStation.isDeleted())
+                            || StringUtils.endsWith(weatherStation.getName(), " Fjärryta")) {
+                        continue;
                     }
-                } else {
-                    style.setLabelText("NODATA");
-                    style.setImageUrl(String.format("%s%s.png", SystemHelper.getPackageAsPath(TrafficInfoPoiProvider.class), "precipitationNoData"));
+
+                    var poi = new MPoi();
+                    poi.setCategory(String.format("%s", "Väderstation"));
+                    poi.setDisplayMarker(true);
+                    poi.setName(weatherStation.getName());
+                    poi.setZoom(0.9);
+                    poi.setPropertyNode(mWeatherView);
+                    poi.setPropertySource(weatherStation);
+                    setLatLonFromGeometry(poi, weatherStation.getGeometry().getWGS84());
+
+                    var style = new MPoiStyle();
+                    poi.setStyle(style);
+                    final var measurement = weatherStation.getMeasurement();
+                    if (weatherStation.isActive()) {
+                        style.setLabelText(String.format("%.0f°", measurement.getAir().getTemp()));
+                        style.setImageUrl(mManager.getIconUrl(measurement.getPrecipitation()));
+                        try {
+                            poi.setDescription(String.format("%s %s",
+                                    measurement.getPrecipitation().getAmount(),
+                                    measurement.getPrecipitation().getAmountName()
+                            ));
+                        } catch (Exception e) {
+                        }
+                    } else {
+                        style.setLabelText("NODATA");
+                        style.setImageUrl(String.format("%s%s.png", SystemHelper.getPackageAsPath(TrafficInfoPoiProvider.class), "precipitationNoData"));
+                    }
+
+                    style.setLabelScale(1.2);
+                    style.setImageScale(FxHelper.getUIScaled(0.1));
+                    style.setLabelVisible(true);
+                    style.setImageLocation(MPoiStyle.ImageLocation.MIDDLE_CENTER);
+
+                    pois.add(poi);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
                 }
-
-                style.setLabelScale(1.2);
-                style.setImageScale(FxHelper.getUIScaled(0.1));
-                style.setLabelVisible(true);
-                style.setImageLocation(MPoiStyle.ImageLocation.MIDDLE_CENTER);
-
-                pois.add(poi);
             }
         });
     }
@@ -197,7 +210,7 @@ public class TrafficInfoPoiProvider implements MPoiProvider {
 
     private void setLatLonFromGeometry(MPoi poi, String wkt) {
         try {
-            Coordinate coordinate = mWktReader.read(wkt).getCoordinate();
+            var coordinate = mWktReader.read(wkt).getCoordinate();
             poi.setLatitude(coordinate.y);
             poi.setLongitude(coordinate.x);
         } catch (ParseException ex) {
