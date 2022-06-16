ab9c0936aa18414299ccfaaa60dd30ffb0d0a1ea
Patrik Karlström <patrik@trixon.se>
2022-06-16 07:46:01 +0200

    wms-stocholm: New module based on Gothenburg

---
+++ modules/wms-stockholm/src/main/java/org/mapton/wms/stockholm/WmsSourceProvider.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2022 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.wms.stockholm;
+
+import org.mapton.api.MWmsSourceProvider;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+@ServiceProvider(service = MWmsSourceProvider.class)
+public class WmsSourceProvider extends MWmsSourceProvider {
+
+    @Override
+    public String getJson() {
+        return loadFromResource("wms_sources.json");
+    }
+
+}
