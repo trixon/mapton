2c2752ce1376152dba8d9da6a1ef55e9356cc2cf
Patrik Karlström <patrik@trixon.se>
2023-11-08 09:17:26 +0100

    butterfly: Topo, Add labels to chart

--- modules-butterfly/topological/src/main/java/org/mapton/butterfly_topo/TopoChartBuilder.java
+++ modules-butterfly/topological/src/main/java/org/mapton/butterfly_topo/TopoChartBuilder.java
@@ -15,26 +15,45 @@
  */
 package org.mapton.butterfly_topo;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.text.SimpleDateFormat;
+import java.util.Objects;
 import java.util.concurrent.Callable;
+import org.apache.commons.lang3.StringUtils;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.block.BlockContainer;
+import org.jfree.chart.block.BorderArrangement;
+import org.jfree.chart.block.EmptyBlock;
+import org.jfree.chart.plot.ValueMarker;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.CompositeTitle;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.chart.ui.HorizontalAlignment;
+import org.jfree.chart.ui.LengthAdjustmentType;
+import org.jfree.chart.ui.RectangleAnchor;
+import org.jfree.chart.ui.RectangleEdge;
 import org.jfree.chart.ui.RectangleInsets;
+import org.jfree.chart.ui.TextAnchor;
+import org.jfree.chart.ui.VerticalAlignment;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 import org.mapton.api.MTemporalManager;
 import org.mapton.api.ui.forms.ChartBuilder;
+import org.mapton.butterfly_alarm.api.AlarmHelper;
+import org.mapton.butterfly_format.types.BComponent;
 import org.mapton.butterfly_format.types.BDimension;
 import org.mapton.butterfly_format.types.controlpoint.BTopoControlPoint;
 import org.mapton.ce_jfreechart.api.ChartHelper;
 import se.trixon.almond.util.DateHelper;
 import se.trixon.almond.util.Dict;
+import se.trixon.almond.util.swing.SwingHelper;
 
 /**
  *
@@ -46,6 +65,8 @@ public class TopoChartBuilder extends ChartBuilder<BTopoControlPoint> {
     private final ChartHelper mChartHelper = new ChartHelper();
     private ChartPanel mChartPanel;
     private final TimeSeriesCollection mDataset = new TimeSeriesCollection();
+    private TextTitle mDateSubTextTitle;
+    private TextTitle mDeltaSubTextTitle;
     private final MTemporalManager mTemporalManager = MTemporalManager.getInstance();
     private final TimeSeries mTimeSeries2d = new TimeSeries("2d");
     private final TimeSeries mTimeSeries3d = new TimeSeries("3d");
@@ -64,11 +85,18 @@ public class TopoChartBuilder extends ChartBuilder<BTopoControlPoint> {
         }
 
         var callable = (Callable<ChartPanel>) () -> {
-            mChart.setTitle(p.getName());
+            setTitle(p);
             updateDataset(p);
             var plot = (XYPlot) mChart.getPlot();
             var dateAxis = (DateAxis) plot.getDomainAxis();
             dateAxis.setRange(DateHelper.convertToDate(mTemporalManager.getLowDate()), DateHelper.convertToDate(mTemporalManager.getHighDate()));
+
+            plot.clearRangeMarkers();
+            plotAlarmIndicators(p);
+
+            var rangeAxis = (NumberAxis) plot.getRangeAxis();
+            rangeAxis.setAutoRange(false);
+            rangeAxis.setRange(-0.050, +0.050);
 
             return mChartPanel;
         };
@@ -107,15 +135,115 @@ public class TopoChartBuilder extends ChartBuilder<BTopoControlPoint> {
         dateAxis.setDateFormatOverride(new SimpleDateFormat("yyyy-MM-dd"));
         dateAxis.setAutoRange(false);
 
-        var rangeAxis = (NumberAxis) plot.getRangeAxis();
-        rangeAxis.setAutoRange(false);
-        rangeAxis.setRange(-0.050, +0.050);
-
         mChartPanel = new ChartPanel(mChart);
         mChartPanel.setMouseZoomable(true, false);
         mChartPanel.setDisplayToolTips(true);
 //        mChartPanel.setDomainZoomable(true);
         mChartPanel.setMouseWheelEnabled(true);
+
+        var font = new Font("monospaced", Font.BOLD, SwingHelper.getUIScaled(12));
+        mDateSubTextTitle = new TextTitle("", font, Color.BLACK, RectangleEdge.TOP, HorizontalAlignment.LEFT, VerticalAlignment.TOP, RectangleInsets.ZERO_INSETS);
+        mDeltaSubTextTitle = new TextTitle("", font, Color.BLACK, RectangleEdge.TOP, HorizontalAlignment.RIGHT, VerticalAlignment.TOP, RectangleInsets.ZERO_INSETS);
+        var blockContainer = new BlockContainer(new BorderArrangement());
+
+        blockContainer.add(mDateSubTextTitle, RectangleEdge.LEFT);
+        blockContainer.add(mDeltaSubTextTitle, RectangleEdge.RIGHT);
+        blockContainer.add(new EmptyBlock(2000, 0));
+        var compositeTitle = new CompositeTitle(blockContainer);
+        compositeTitle.setPadding(new RectangleInsets(0, 20, 0, 20));
+        mChart.addSubtitle(compositeTitle);
+    }
+
+    private void plotAlarmIndicator(BComponent component, double value, Color color) {
+        var marker = new ValueMarker(value);
+        float width = 1.0f;
+        float dash[] = {5.0f, 5.0f};
+        var dashedStroke = new BasicStroke(width, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER, 1.5f, dash, 0);
+        var stroke = new BasicStroke(width, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER, 1.5f, null, 0);
+        if (component == BComponent.HEIGHT) {
+            marker.setStroke(dashedStroke);
+        } else {
+            marker.setStroke(stroke);
+        }
+        marker.setLabelOffsetType(LengthAdjustmentType.EXPAND);
+        marker.setPaint(color);
+
+        var plot = (XYPlot) mChart.getPlot();
+        plot.addRangeMarker(marker);
+    }
+
+    private void plotAlarmIndicators(BTopoControlPoint p) {
+        var ha = p.ext().getAlarm(BComponent.HEIGHT);
+        if (ha != null) {
+            var range0 = ha.ext().getRange0();
+            if (range0 != null) {
+                plotAlarmIndicator(BComponent.HEIGHT, range0.getMinimum(), Color.YELLOW);
+                plotAlarmIndicator(BComponent.HEIGHT, range0.getMaximum(), Color.YELLOW);
+            }
+
+            var range1 = ha.ext().getRange1();
+            if (range1 != null) {
+                plotAlarmIndicator(BComponent.HEIGHT, range1.getMinimum(), Color.RED);
+                plotAlarmIndicator(BComponent.HEIGHT, range1.getMaximum(), Color.RED);
+            }
+        }
+
+        var pa = p.ext().getAlarm(BComponent.PLANE);
+        if (pa != null) {
+            var range0 = pa.ext().getRange0();
+            if (range0 != null) {
+                plotAlarmIndicator(BComponent.PLANE, range0.getMinimum(), Color.YELLOW);
+                plotAlarmIndicator(BComponent.PLANE, range0.getMaximum(), Color.YELLOW);
+            }
+
+            var range1 = pa.ext().getRange1();
+            if (range1 != null) {
+                plotAlarmIndicator(BComponent.PLANE, range1.getMinimum(), Color.RED);
+                plotAlarmIndicator(BComponent.PLANE, range1.getMaximum(), Color.RED);
+            }
+        }
+    }
+
+    private void setTitle(BTopoControlPoint p) {
+        mChart.setTitle(p.getName());
+        mChart.getTitle().setBackgroundPaint(Color.GRAY);
+        mChart.getTitle().setPaint(TopoHelper.getAlarmColorAwt(p));
+        var dateFirst = Objects.toString(DateHelper.toDateString(p.ext().getObservationRawFirstDate()), "");
+        var dateLast = Objects.toString(DateHelper.toDateString(p.ext().getObservationRawLastDate()), "");
+        var date = "%s → %s".formatted(dateFirst, dateLast);
+        mDateSubTextTitle.setText(date);
+
+        var sb = new StringBuilder();
+        if (!StringUtils.isBlank(p.getNameOfAlarmHeight())) {
+            sb.append("H ").append(p.getNameOfAlarmHeight());
+            if (!StringUtils.isBlank(p.getNameOfAlarmPlane())) {
+                sb.append(", ");
+            }
+        }
+
+        if (!StringUtils.isBlank(p.getNameOfAlarmPlane())) {
+            sb.append("P ").append(p.getNameOfAlarmPlane());
+        }
+
+        var alarmNames = sb.toString();
+
+        String hAlarm = "";
+        if (p.getDimension() != BDimension._2d) {
+            hAlarm = "H " + AlarmHelper.getInstance().getLimitsAsString(BComponent.HEIGHT, p);
+            if (p.getDimension() == BDimension._3d) {
+                hAlarm = hAlarm + ", ";
+            }
+        }
+
+        String pAlarm = "";
+        if (p.getDimension() != BDimension._1d) {
+            pAlarm = "P " + AlarmHelper.getInstance().getLimitsAsString(BComponent.PLANE, p);
+        }
+
+        String delta = p.ext().deltaZero().getDelta(3);
+
+        var rightTitle = "%s%s: %s".formatted(hAlarm, pAlarm, delta);
+        mDeltaSubTextTitle.setText(rightTitle);
     }
 
     private void updateDataset(BTopoControlPoint p) {
@@ -126,8 +254,27 @@ public class TopoChartBuilder extends ChartBuilder<BTopoControlPoint> {
         mTimeSeries2d.clear();
         mTimeSeries3d.clear();
 
+        var plot = (XYPlot) mChart.getPlot();
+        plot.clearDomainMarkers();
+
         p.ext().getObservationsTimeFiltered().forEach(o -> {
             var minute = mChartHelper.convertToMinute(o.getDate());
+            if (o.isReplacementMeasurement()) {
+                var marker = new ValueMarker(minute.getFirstMillisecond());
+                marker.setPaint(Color.RED);
+                marker.setLabel("E");
+                marker.setLabelAnchor(RectangleAnchor.TOP_RIGHT);
+                marker.setLabelTextAnchor(TextAnchor.TOP_LEFT);
+                plot.addDomainMarker(marker);
+            } else if (o.isZeroMeasurement()) {
+                var marker = new ValueMarker(minute.getFirstMillisecond());
+                marker.setPaint(Color.BLUE);
+                marker.setLabel("N");
+                marker.setLabelAnchor(RectangleAnchor.TOP_RIGHT);
+                marker.setLabelTextAnchor(TextAnchor.TOP_LEFT);
+                plot.addDomainMarker(marker);
+            }
+
             if (p.getDimension() == BDimension._1d || p.getDimension() == BDimension._3d) {
                 mTimeSeriesH.add(minute, o.ext().getDeltaZ());
             }
