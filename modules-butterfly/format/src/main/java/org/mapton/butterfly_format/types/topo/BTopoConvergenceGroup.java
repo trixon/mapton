159b98eaf03548219d4e396731359a34f185a965
Patrik Karlström <patrik@trixon.se>
2025-03-31 14:35:54 +0200

    butterfly: Format, Add anchor point support

--- modules-butterfly/format/src/main/java/org/mapton/butterfly_format/types/topo/BTopoConvergenceGroup.java
+++ modules-butterfly/format/src/main/java/org/mapton/butterfly_format/types/topo/BTopoConvergenceGroup.java
@@ -16,6 +16,9 @@
 package org.mapton.butterfly_format.types.topo;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import javafx.geometry.Point2D;
+import javafx.geometry.Point3D;
 import org.mapton.butterfly_format.types.BXyzPoint;
 
 /**
@@ -54,11 +57,20 @@ public class BTopoConvergenceGroup extends BTopoControlPoint {
 
     public class Ext extends BXyzPoint.Ext<BTopoControlPointObservation> {
 
+        private BTopoControlPoint mAnchorPoint;
         private ArrayList<BTopoControlPoint> mControlPoints = new ArrayList<>();
         private ArrayList<BTopoConvergencePair> mPairs = new ArrayList<>();
 
+        public BTopoControlPoint getAnchorPoint() {
+            return mAnchorPoint;
+        }
+
         public ArrayList<BTopoControlPoint> getControlPoints() {
             return mControlPoints;
+        }
+
+        public ArrayList<BTopoControlPoint> getControlPointsWithoutAnchor() {
+            return new ArrayList<>(mControlPoints.stream().filter(p -> p != getAnchorPoint()).toList());
         }
 
         public BTopoConvergencePair getMaxDeltaDistanceOverTime() {
@@ -94,28 +106,66 @@ public class BTopoConvergenceGroup extends BTopoControlPoint {
             return storedPair;
         }
 
-//        @Override
-//        public int getNumOfObservations() {
-        ////            return getObservationsAllRaw().stream()
-////                    .map(o -> o.getDate().toString())
-////                    .collect(Collectors.toSet()).size();
-//            return 1 + getObservationsAllRaw().size();
-//        }
-//
-//        @Override
-//        public int getNumOfObservationsFiltered() {
-//            return getObservationsTimeFiltered().stream()
-//                    .map(o -> o.getDate().toString())
-//                    .collect(Collectors.toSet()).size();
-//        }
-
         public ArrayList<BTopoConvergencePair> getPairs() {
             return mPairs;
         }
 
+        public ArrayList<Point2D> getProjected2dCoordinates() {
+            var viewDistance = 50.0;
+            var points2d = new ArrayList<Point2D>();
+            var points3d = getControlPointsWithoutAnchor().stream()
+                    .map(p1 -> new Point3D(p1.getZeroX(), p1.getZeroY(), p1.getZeroZ()))
+                    .toList();
+
+            for (var point : points3d) {
+                var x2d = (point.getX() * viewDistance) / point.getY();
+                var y2d = (point.getZ() * viewDistance) / point.getY();
+                points2d.add(new Point2D(x2d, y2d));
+            }
+
+            var avgX = points3d.stream().mapToDouble(p -> p.getX()).average().orElse(0.0);
+            var avgY = points3d.stream().mapToDouble(p -> p.getY()).average().orElse(0.0);
+            points2d = points2d.stream()
+                    .map(p -> {
+                        return p.subtract(avgX, avgY);
+                    }).collect(Collectors.toCollection(ArrayList::new));
+
+            return points2d;
+        }
+
+        public boolean hasAnchorPoint() {
+            return mAnchorPoint != null;
+        }
+
         public void setControlPoints(ArrayList<BTopoControlPoint> controlPoints) {
             mControlPoints = controlPoints;
+            if (controlPoints.isEmpty()) {
+                return;
+            }
+            var maxDistance = 0.0;
+            var totalDistance = 0.0;
+            mAnchorPoint = null;
+
+            for (var p1 : controlPoints) {
+                var pp1 = new Point3D(p1.getZeroX(), p1.getZeroY(), p1.getZeroZ());
+                var pointDistance = 0.0;
+                for (var p2 : controlPoints) {
+                    var pp2 = new Point3D(p2.getZeroX(), p2.getZeroY(), p2.getZeroZ());
+                    pointDistance += pp1.distance(pp2);
+                }
+                totalDistance += pointDistance;
+                if (pointDistance > maxDistance) {
+                    maxDistance = pointDistance;
+                    mAnchorPoint = p1;
+                }
+            }
+
+            var avgDistance = (totalDistance - maxDistance) / controlPoints.size();
+            if (maxDistance < avgDistance * 2) {
+                mAnchorPoint = null;
+            }
         }
 
     }
+
 }
