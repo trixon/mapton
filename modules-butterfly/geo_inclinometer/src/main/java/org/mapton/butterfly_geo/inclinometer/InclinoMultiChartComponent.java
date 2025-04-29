e01e66411f5569da305dc487c885f0902a262800
Patrik Karlström <patrik@trixon.se>
2025-04-29 13:39:24 +0200

    butterfly: Inclino, provide MultiChartComponent

---
+++ modules-butterfly/geo_inclinometer/src/main/java/org/mapton/butterfly_geo/inclinometer/InclinoMultiChartComponent.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2025 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.butterfly_geo.inclinometer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import org.mapton.api.MLatLon;
+import org.mapton.butterfly_core.api.BMultiChartComponent;
+import org.mapton.butterfly_format.types.geo.BGeoInclinometerPoint;
+import org.mapton.butterfly_format.types.geo.BGeoInclinometerPointObservation;
+import org.openide.util.lookup.ServiceProvider;
+import se.trixon.almond.util.DateHelper;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+@ServiceProvider(service = BMultiChartComponent.class)
+public class InclinoMultiChartComponent extends BMultiChartComponent {
+
+    public InclinoMultiChartComponent() {
+    }
+
+    @Override
+    public String getAxisLabel() {
+        return "mm";
+    }
+
+    @Override
+    public String getDecimalPattern() {
+        return "0.0";
+    }
+
+    @Override
+    public String getName() {
+        return "Inklinometrar";
+    }
+
+    @Override
+    public ArrayList<BGeoInclinometerPoint> getPointsAndSeries(MLatLon latLon, LocalDate firstDate, LocalDate lastDate) {
+        var pointList = InclinoManager.getInstance().getTimeFilteredItems().stream()
+                .filter(p -> {
+                    return latLon.distance(new MLatLon(p.getLat(), p.getLon())) <= DISTANCE_BLAST;
+                })
+                .filter(p -> {
+                    try {
+                        if (p.ext().getDateFirst().toLocalDate().isAfter(lastDate)
+                                || p.ext().getDateLatest().toLocalDate().isBefore(firstDate)) {
+                            return false;
+                        }
+                    } catch (Exception e) {
+                        return false;
+                    }
+                    return true;
+                })
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        System.out.println("FILTERED INCLINOMETERS");
+        for (var p : pointList) {
+            System.out.println(p.getName());
+        }
+
+        var pointsToExclude = new ArrayList<BGeoInclinometerPoint>();
+        for (var p : pointList) {
+            var observations = p.ext().getObservationsTimeFiltered().stream()
+                    .filter(o -> DateHelper.isBetween(firstDate, lastDate, o.getDate().toLocalDate()))
+                    .filter(o -> o.getMeasuredZ() != null)
+                    .map(o -> {
+                        var oo = new BGeoInclinometerPointObservation();
+                        oo.setDate(o.getDate());
+                        oo.setMeasuredZ(o.getMeasuredZ());
+                        return oo;
+                    })
+                    .toList();
+
+            if (observations.size() > 1) {
+                var map = new TreeMap<LocalDateTime, Double>();
+                for (var o : observations) {
+                    map.put(o.getDate(), o.getMeasuredZ() - observations.getFirst().getMeasuredZ());
+                }
+                p.setValue(BMultiChartComponent.class, map);
+            } else {
+                pointsToExclude.add(p);
+            }
+        }
+
+        pointList.removeAll(pointsToExclude);
+        sortPointList(pointList);
+
+        return pointList;
+    }
+
+}
