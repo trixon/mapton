c15e1f7e40ce67ed6b9f415b3ef9ac932678ed99
Patrik Karlström <patrik@trixon.se>
2026-01-23 09:42:32 +0100

    butterfly: Format, add GeoPreDrilling WIP

---
+++ modules-butterfly/format/src/main/java/org/mapton/butterfly_format/types/geo/BGeoObjectPoint.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.butterfly_format.types.geo;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.mapton.butterfly_format.types.BXyzPoint;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+@JsonPropertyOrder({
+    "id",
+    "name",
+    "group",
+    "comment",
+    "lat",
+    "lon"
+})
+public abstract class BGeoObjectPoint extends BXyzPoint {
+
+}
