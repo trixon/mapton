54e035d2a769fd02df25740dad872430a1989f6b
Patrik Karlström <patrik@trixon.se>
2026-05-18 10:53:36 +0200

    butterfly: Remove obsolete chart mouse listener

--- modules-butterfly/structural_load/src/main/java/org/mapton/butterfly_structural/load/chart/MultiChartBuilder.java
+++ modules-butterfly/structural_load/src/main/java/org/mapton/butterfly_structural/load/chart/MultiChartBuilder.java
@@ -15,18 +15,11 @@
  */
 package org.mapton.butterfly_structural.load.chart;
 
-import com.sun.jna.platform.KeyboardUtils;
-import java.awt.event.KeyEvent;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.TreeMap;
 import java.util.concurrent.Callable;
-import org.jfree.chart.ChartMouseEvent;
-import org.jfree.chart.ChartMouseListener;
 import org.jfree.chart.ChartPanel;
-import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.entity.LegendItemEntity;
-import org.jfree.chart.entity.XYItemEntity;
 import org.jfree.data.time.TimeSeries;
 import org.mapton.butterfly_core.api.BCoordinatrix;
 import org.mapton.butterfly_core.api.BMultiChartPart;
@@ -34,7 +27,6 @@ import org.mapton.butterfly_core.api.XyzChartBuilder;
 import org.mapton.butterfly_format.types.BXyzPoint;
 import org.mapton.butterfly_format.types.structural.BStructuralLoadCellPoint;
 import org.mapton.ce_jfreechart.api.ChartHelper;
-import se.trixon.almond.util.DateHelper;
 
 /**
  *
@@ -65,37 +57,7 @@ public class MultiChartBuilder extends XyzChartBuilder<BStructuralLoadCellPoint>
             setTitle(p);
             updateDataset(p);
             var plot = getPlot();
-            var dateAxis = (DateAxis) plot.getDomainAxis();
-            dateAxis.setRange(DateHelper.convertToDate(mDateFirst), DateHelper.convertToDate(mDateLast));
             plot.clearRangeMarkers();
-
-            getChartPanel().addChartMouseListener(new ChartMouseListener() {
-                @Override
-                public void chartMouseClicked(ChartMouseEvent event) {
-                    var e = event.getEntity();
-                    if (e != null) {
-                        var name = "";
-                        if (event.getEntity() instanceof XYItemEntity entity) {
-                            name = getDataset().getSeriesKey(entity.getSeriesIndex()).toString();
-                        } else if (e instanceof LegendItemEntity entity) {
-                            name = entity.getSeriesKey().toString();
-                        }
-
-                        if (!name.isBlank()) {
-                            var isKeyPressed = KeyboardUtils.isPressed(KeyEvent.VK_SHIFT);
-                            mMultiChartComponent.panTo(name);
-                            if (isKeyPressed) {
-                                mMultiChartComponent.select(name);
-                            }
-                        }
-                    }
-                }
-
-                @Override
-                public void chartMouseMoved(ChartMouseEvent event) {
-                    //nvm
-                }
-            });
 
             return getChartPanel();
         };
