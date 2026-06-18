acf807708d648e87ae0125f6b23f279529aa621c
Patrik Karlström <patrik@trixon.se>
2026-06-18 13:00:43 +0200

    butterfly: Insar, add chart overlay

---
+++ modules-butterfly/remote_insar/src/main/java/org/mapton/butterfly_remote/insar/chart/overlay/InsarChartOverlay.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2026 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.butterfly_remote.insar.chart.overlay;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.time.LocalDate;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.mapton.api.MChartOverlay;
+import org.mapton.butterfly_core.api.BChartOverlay;
+import org.mapton.butterfly_core.api.ButterflyHelper;
+import org.mapton.butterfly_format.types.BBasePoint;
+import org.mapton.butterfly_remote.insar.InsarHelper;
+import org.mapton.ce_jfreechart.api.ChartHelper;
+import org.openide.util.lookup.ServiceProvider;
+import se.trixon.almond.util.GraphicsHelper;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+@ServiceProvider(service = MChartOverlay.class)
+public class InsarChartOverlay extends BChartOverlay {
+
+    public static final Color COLOR = Color.decode("#FFFF00");
+    public static final int MAX_COUNT = 5;
+    public static final int MAX_DISTANCE = 20;
+
+    public InsarChartOverlay() {
+        super("InSAR");
+    }
+
+    @Override
+    public synchronized void plot(XYPlot plot, BBasePoint p, LocalDate aStartDate) {
+        resetDatasetIfExisting(plot, mIndex);
+
+        if (mObjectStorageManager.getBoolean(InsarChartSOSB.class, InsarChartSOSB.DEFAULT_VALUE)) {
+            var startDate = aStartDate == null ? LocalDate.now() : aStartDate;
+            var points = ButterflyHelper.getLimitedPoints(p, p.getButterfly().remote().getInsarPoints(), true, MAX_DISTANCE, MAX_COUNT, startDate);
+            if (!points.isEmpty()) {
+                var renderer = new XYLineAndShapeRenderer(true, false);
+                var dataset = new TimeSeriesCollection();
+                init(plot, dataset, renderer);
+
+                var color = GraphicsHelper.colorAddAlpha(COLOR, 80);
+
+                for (int i = 0; i < points.size(); i++) {
+                    if (i > 0) {
+                        color = GraphicsHelper.brighten(color, 0.25);
+                    }
+                    var point = points.get(i);
+                    var distance = point.<Double>getValue(ButterflyHelper.KEY_DISTANCE);
+                    var timeSeries = new TimeSeries("%c.%.0f".formatted('A' + i, distance));
+
+                    for (var o : point.ext().getObservationsTimeFiltered()) {
+                        if (o.getDate().isAfter(startDate.atStartOfDay())) {
+                            var delta = o.ext().getDeltaZ() * InsarHelper.getScaleFactor(point);
+
+                            timeSeries.addOrUpdate(ChartHelper.convertToMinute(o.getDate()), delta);
+                        }
+                    }
+
+                    dataset.addSeries(timeSeries);
+                    var seriesIndex = dataset.getSeriesIndex(timeSeries.getKey());
+                    if (seriesIndex >= 0) {
+                        renderer.setSeriesToolTipGenerator(seriesIndex, (xyDataset, series, item) -> {
+                            return "%.0fm  %s".formatted(distance, point.getName());
+                        });
+
+                        renderer.setSeriesVisibleInLegend(seriesIndex, true);
+                        renderer.setSeriesPaint(seriesIndex, color, false);
+                        var width = i == 0 ? 3f : 1.5f;
+                        renderer.setSeriesStroke(seriesIndex, new BasicStroke(width));
+                    }
+                }
+            }
+        }
+    }
+}
