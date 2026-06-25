983d5e07702fe43b4b933e063b14dba3ef0ea7f9
Patrik Karlström <patrik@trixon.se>
2026-06-25 14:00:35 +0200

    core: Tweak limit filter popover

--- modules-butterfly/core/src/main/java/org/mapton/butterfly_core/api/BContentView.java
+++ modules-butterfly/core/src/main/java/org/mapton/butterfly_core/api/BContentView.java
@@ -16,6 +16,7 @@
 package org.mapton.butterfly_core.api;
 
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
 import javafx.scene.layout.VBox;
 import org.mapton.api.ui.forms.SingleListForm;
 import org.mapton.core.api.ui.MPresetPopOver;
@@ -50,6 +51,23 @@ public abstract class BContentView {
         mListSortOrderScb.getItems().setAll(BListSortOrder.values());
         mListSortOrderScb.valueProperty().bindBidirectional(mContentOptions.listSortOrderProperty());
 
+        mListLimitComboBox.setCellFactory(lv -> new ListCell<Integer>() {
+            @Override
+            protected void updateItem(Integer item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    if (item == 0) {
+                        setText("Obegränsat antal objekt");
+                    } else {
+                        setText("Max %d objekt".formatted(item));
+                    }
+                }
+            }
+        });
+
+        mListLimitComboBox.setButtonCell(mListLimitComboBox.getCellFactory().call(null));
         mListLimitComboBox.getItems().setAll(0, 5, 10, 25, 50, 100, 1000, 10000);
         mListLimitComboBox.setValue(mContentOptions.getListLimitRaw());
         mListLimitComboBox.valueProperty().addListener((p, o, n) -> {
