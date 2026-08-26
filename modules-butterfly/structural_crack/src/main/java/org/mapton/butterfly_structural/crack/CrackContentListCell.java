c67189cea701a3c77683ca412f7c7206ee7a072e
Patrik Karlström <patrik@trixon.se>
2026-08-26 13:36:46 +0200

    butterfly: Crack, simplify ListCell

--- modules-butterfly/structural_crack/src/main/java/org/mapton/butterfly_structural/crack/CrackContentListCell.java
+++ modules-butterfly/structural_crack/src/main/java/org/mapton/butterfly_structural/crack/CrackContentListCell.java
@@ -45,11 +45,6 @@ class CrackContentListCell extends BContentListCell<BStructuralCrackPoint> {
         loadTooltip(p);
         mAlarmIndicator.update(p);
 
-        var header = p.getName();
-        if (StringUtils.isNotBlank(p.getStatus())) {
-            header = "%s [%s]".formatted(header, p.getStatus());
-        }
-
         var sign = "⇐";
         var desc1 = "%s: %s".formatted(StringUtils.defaultIfBlank(p.getCategory(), "NOVALUE"), p.getAlarm1Id());
         var dateSB = new StringBuilder(StringHelper.toString(p.getDateLatest() == null ? null : p.getDateLatest().toLocalDate(), "NOVALUE"));
@@ -70,7 +65,7 @@ class CrackContentListCell extends BContentListCell<BStructuralCrackPoint> {
         var dateZero = StringHelper.toString(p.getDateZero(), "NOVALUE");
         var dz = p.ext().deltaZero().getDeltaZ() * 1000;
         var desc4 = "%s: %.2f mm".formatted(dateZero, dz);
-        mHeaderLabel.setText(header);
+        mHeaderLabel.setText(getHeader(p));
         mDesc1Label.setText(desc1);
         mDesc2Label.setText(dateSB.toString());
 //        mDesc3Label.setText(desc3);
--- modules-butterfly/structural_crack/src/main/java/org/mapton/butterfly_structural/crack/CrackContentView.java
+++ modules-butterfly/structural_crack/src/main/java/org/mapton/butterfly_structural/crack/CrackContentView.java
@@ -45,6 +45,7 @@ public class CrackContentView extends BContentView {
         mToolBarPopOver.setToolBar(ActionUtils.createToolBar(subToolBarActions, ActionUtils.ActionTextBehavior.SHOW));
 
         mListForm = new SingleListForm<>(mManager, Bundle.CTL_CrackAction());
+        bindFooterLabel(mListForm);
         var listFormConfiguration = new ListFormConfiguration()
                 .setUseTextFilter(true)
                 .setToolbarActions(getDefaultToolBarActions());
