c0b1a14d84d895b29a67f7a3cdbea196d957f877
Patrik Karlström <patrik@trixon.se>
2026-08-31 13:01:02 +0200

    butterfly: Core, make ListSortOrder null safe

--- modules-butterfly/core/src/main/java/org/mapton/butterfly_core/api/BListSortOrder.java
+++ modules-butterfly/core/src/main/java/org/mapton/butterfly_core/api/BListSortOrder.java
@@ -16,6 +16,10 @@
 package org.mapton.butterfly_core.api;
 
 import java.util.Comparator;
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.naturalOrder;
+import static java.util.Comparator.nullsLast;
+import static java.util.Comparator.reverseOrder;
 import org.mapton.butterfly_format.types.BXyzPoint;
 
 /**
@@ -24,49 +28,51 @@ import org.mapton.butterfly_format.types.BXyzPoint;
  */
 public enum BListSortOrder {
     STANDARD(Order.ASC, "Ursprung och namn",
-            Comparator.comparing(BXyzPoint::getOrigin).thenComparing(Comparator.comparing(BXyzPoint::getName))),
+            comparing(BXyzPoint::getOrigin).thenComparing(comparing(BXyzPoint::getName))),
     NAME(Order.ASC, "Namn",
-            Comparator.comparing(BXyzPoint::getName)),
+            comparing(BXyzPoint::getName)),
     DELTA_1D(Order.DESC, "Δ1d",
-            Comparator.comparing((BXyzPoint p) -> {
+            comparing((BXyzPoint p) -> {
                 var value = p.extOrNull().deltaZero().getDelta1();
                 return (value == null) ? null : Math.abs(value);
-            }, Comparator.nullsLast(Comparator.reverseOrder()))),
+            }, nullsLast(reverseOrder()))),
     DELTA_2D(Order.DESC, "Δ2d",
-            Comparator.comparing((BXyzPoint p) -> {
+            comparing((BXyzPoint p) -> {
                 var value = p.extOrNull().deltaZero().getDelta2();
                 return (value == null) ? null : Math.abs(value);
-            }, Comparator.nullsLast(Comparator.reverseOrder()))),
+            }, nullsLast(reverseOrder()))),
     DELTA_3D(Order.DESC, "Δ3d",
-            Comparator.comparing((BXyzPoint p) -> {
+            comparing((BXyzPoint p) -> {
                 var value = p.extOrNull().deltaZero().getDelta3();
                 return (value == null) ? null : Math.abs(value);
-            }, Comparator.nullsLast(Comparator.reverseOrder()))),
+            }, nullsLast(reverseOrder()))),
     Z_ASC(Order.ASC, "Z",
-            Comparator.comparing((BXyzPoint p) -> {
+            comparing((BXyzPoint p) -> {
                 return p.getZeroZ();
-            }, Comparator.nullsLast(Comparator.naturalOrder()))),
+            }, nullsLast(naturalOrder()))),
     Z_DESC(Order.DESC, "Z",
-            Comparator.comparing((BXyzPoint p) -> {
+            comparing((BXyzPoint p) -> {
                 return p.getZeroZ();
-            }, Comparator.nullsLast(Comparator.reverseOrder()))),
+            }, nullsLast(reverseOrder()))),
     DATE_PREV_ASC(Order.ASC, "Datum, senaste",
-            Comparator.comparing(BXyzPoint::getDateLatest, Comparator.nullsLast(Comparator.naturalOrder()))),
+            comparing(BXyzPoint::getDateLatest, nullsLast(naturalOrder()))),
     DATE_PREV_DESC(Order.DESC, "Datum, senaste",
-            Comparator.comparing(BXyzPoint::getDateLatest, Comparator.nullsLast(Comparator.reverseOrder()))),
+            comparing(BXyzPoint::getDateLatest, nullsLast(reverseOrder()))),
     DATE_NEXT_ASC(Order.ASC, "Datum, nästa",
-            Comparator.comparing((BXyzPoint p) -> p.extOrNull().getObservationRawNextDate(), Comparator.nullsLast(Comparator.naturalOrder()))),
+            comparing((BXyzPoint p) -> p.extOrNull().getObservationRawNextDate(), nullsLast(naturalOrder()))),
     DATE_NEXT_DESC(Order.DESC, "Datum, nästa",
-            Comparator.comparing((BXyzPoint p) -> p.extOrNull().getObservationRawNextDate(), Comparator.nullsLast(Comparator.reverseOrder()))),
+            comparing((BXyzPoint p) -> p.extOrNull().getObservationRawNextDate(), nullsLast(reverseOrder()))),
     NORTH_SOUTH(Order.NONE, "Nord-Syd, Väst-Öst",
-            Comparator.comparingDouble(BXyzPoint::getZeroY).reversed().thenComparing(Comparator.comparingDouble(BXyzPoint::getZeroX))),
+            comparing(BXyzPoint::getZeroY, nullsLast(reverseOrder()))
+                    .thenComparing(BXyzPoint::getZeroX, nullsLast(naturalOrder()))),
     WEST_EAST(Order.NONE, "Väst-Öst, Nord-Syd",
-            Comparator.comparingDouble(BXyzPoint::getZeroX).thenComparing(Comparator.comparingDouble(BXyzPoint::getZeroY).reversed()));
-    private final Comparator mComparator;
+            comparing(BXyzPoint::getZeroX, nullsLast(naturalOrder()))
+                    .thenComparing(BXyzPoint::getZeroY, nullsLast(reverseOrder())));
+    private final Comparator<BXyzPoint> mComparator;
     private final String mName;
     private final Order mOrder;
 
-    private BListSortOrder(Order order, String name, Comparator comparator) {
+    private BListSortOrder(Order order, String name, Comparator<BXyzPoint> comparator) {
         mOrder = order;
         mName = name;
         mComparator = comparator;
