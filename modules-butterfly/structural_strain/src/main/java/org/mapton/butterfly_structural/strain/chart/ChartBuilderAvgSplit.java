9e971c0763b62c88fb8905975a814496c8d1a650
Patrik Karlström <patrik@trixon.se>
2026-09-15 13:49:48 +0200

    butterfly: Strain, simplify chart avg and marker plot

---
+++ modules-butterfly/structural_strain/src/main/java/org/mapton/butterfly_structural/strain/chart/ChartBuilderAvgSplit.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2025 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapton.butterfly_structural.strain.chart;
+
+import java.util.concurrent.Callable;
+import javax.swing.JPanel;
+import org.mapton.butterfly_core.api.BChartSplit;
+import org.mapton.butterfly_format.types.structural.BStructuralStrainGaugePoint;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+public class ChartBuilderAvgSplit extends BChartSplit {
+
+    private final ChartBuilderDelta mCompleteChartBuilder;
+    private final ChartBuilderDelta mLatestChartBuilder;
+
+    public ChartBuilderAvgSplit() {
+        mLatestChartBuilder = new ChartBuilderDelta(true, 28);
+        mCompleteChartBuilder = new ChartBuilderDelta(true, null);
+    }
+
+    public synchronized Callable<JPanel> build(BStructuralStrainGaugePoint p) {
+        return build(p, true);
+    }
+
+    public synchronized Callable<JPanel> build(BStructuralStrainGaugePoint p, boolean standardLayout) {
+        if (p == null) {
+            return null;
+        }
+
+        var callable = (Callable<JPanel>) () -> {
+            return createSplitPanel(
+                    mLatestChartBuilder.build(p).call(),
+                    mCompleteChartBuilder.build(p).call()
+            );
+        };
+
+        return callable;
+    }
+}
